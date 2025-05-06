4be1db06ecfe01c5e65a87a3e7c4b611ee295082
nanashi <egorglazkov71@gmail.com>
2025-05-06 20:20:56 +0300

    feat: implement quick sort

--- SortStrategy/QuickSortStrategy.cs
+++ SortStrategy/QuickSortStrategy.cs
@@ -5,9 +5,46 @@ namespace StrategyPattern.Sort
 {
     public class QuickSortStrategy : ISortStrategy
     {
-        public List<int> Sort(List<int> numbers)
+        public void Sort(List<int> array)
         {
-            throw new NotImplementedException();
+            QuickSortAlgorithm(array, 0, array.Count - 1);
+        }
+
+        private static void QuickSortAlgorithm(List<int> array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(array, left, right);
+
+                QuickSortAlgorithm(array, left, pivotIndex - 1);
+                QuickSortAlgorithm(array, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(List<int> array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, right);
+
+            return i + 1;
+        }
+
+        private static void Swap(List<int> array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
 }
