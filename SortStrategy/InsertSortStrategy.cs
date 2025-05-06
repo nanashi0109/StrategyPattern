f56c6c83cf868838ef6d126aaf65e1ffa6ea9cd1
nanashi <egorglazkov71@gmail.com>
2025-05-06 20:17:21 +0300

    feat: implement insert sort

--- SortStrategy/InsertSortStrategy.cs
+++ SortStrategy/InsertSortStrategy.cs
@@ -5,9 +5,21 @@ namespace StrategyPattern.Sort
 {
     public class InsertSortStrategy : ISortStrategy
     {
-        public List<int> Sort(List<int> numbers)
+        public void Sort(List<int> array)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i < array.Count; i++)
+            {
+                int current = array[i];  
+                int j = i - 1;         
+
+                while (j >= 0 && array[j] > current)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = current;
+            }
         }
     }
 }
