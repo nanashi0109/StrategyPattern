3a7960b2a47d5f21c58dde534f02c94b7a9d0885
nanashi <egorglazkov71@gmail.com>
2025-05-06 19:59:00 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            Console.ReadKey();
+        }
+    }
+}
