3a6e7a51dbfa1062025c02aa131412f4d7df5e19
nanashi <egorglazkov71@gmail.com>
2025-05-06 20:10:13 +0300

    feat: add quick sort strategy

---
+++ SortStrategy/QuickSortStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrategyPattern.Sort
+{
+    public class QuickSortStrategy : ISortStrategy
+    {
+        public List<int> Sort(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
